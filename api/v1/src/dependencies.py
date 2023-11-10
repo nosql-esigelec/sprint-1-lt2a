c6aaadf16a07a3411d5f86a7f559e6cdfb5a1af2
el-tegy <elisee.tegue@groupe-esigelec.org>
2023-11-10 19:18:31 +0100

    feat(mongo): add get_mongo_db function

--- api/v1/src/dependencies.py
+++ api/v1/src/dependencies.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
-from db.mongo_db import MongoDB
+from api.v1.src.db.mongo_db import MongoDB
 # from src.db.neo4j_db import Neo4jDB
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '..', '..', '.env')
