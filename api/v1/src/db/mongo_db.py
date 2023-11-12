11ad962ec61e26f64e8157d1ec02a89ef9718393
el-tegy <elisee.tegue@groupe-esigelec.org>
2023-11-12 23:32:59 +0100

    feat(mongo): Add pymongo import and implement create method for new collections

--- api/v1/src/db/mongo_db.py
+++ api/v1/src/db/mongo_db.py
@@ -12,7 +12,7 @@ Classes:
 from typing import Optional, List, Union
 #import datetime
 #from bson.objectid import ObjectId
-#from pymongo import errors
+from pymongo import errors
 from api.v1.src.utils.handlers import handle_db_operations
 
 
@@ -43,11 +43,11 @@ class MongoDB:
 
         drop_result = self.db[collection_name].drop()
         return drop_result
-    
+
     @handle_db_operations
     def drop_database(self,
-                      database_name: str
-                      ) -> dict:
+                    database_name: str
+                ) -> dict:
         """
         Drop a database from the database.
 
@@ -63,9 +63,9 @@ class MongoDB:
 
     @handle_db_operations
     def drop_index(self,
-                   collection_name: str,
-                   index_name: str
-                   ) -> dict:
+                collection_name: str,
+                index_name: str
+            ) -> dict:
         """
         Drop an index from the collection.
 
@@ -102,13 +102,13 @@ class MongoDB:
         collection = self.db[collection_name]
         index_name = collection.create_index([(field, 1)], unique=unique)
         return index_name
-    
+
     @handle_db_operations
     def create(self,
-               document_data: dict,
-               collection_name: str,
-               many: bool = False
-               ) -> Union[str, List[str]]:
+            document_data: dict,
+            collection_name: str,
+            many: bool = False
+            ) -> Union[str, List[str]]:
         """
         Create a document in a collection in the database.
         Used for create_org, create_project
@@ -121,6 +121,9 @@ class MongoDB:
         Returns:
             Union[str, List[str]]: The inserted document's ID or list of IDs.
         """
+        if collection_name not in self.db.list_collection_names():
+            # Create a new collection by inserting a document
+            self.db[collection_name].insert_one({"username":"ana", "name": "Anna", "age": 20})
         collection = self.db[collection_name]
         if many:
             insert_result = collection.insert_many(document_data)
@@ -171,13 +174,7 @@ class MongoDB:
             return document
 
     @handle_db_operations
-    def update(
-        self,
-        query: dict,
-        document_data: dict,
-        collection_name: str,
-        update_type: str = "set",
-    ) -> int:
+    def update(self, query: dict, document_data: dict, collection_name: str, update_type: str = "set") -> dict:
         """
         Update a document in a given collection in the database.
 
@@ -188,27 +185,39 @@ class MongoDB:
             update_type (str): The type of update to be performed. Defaults to "set".
 
         Returns:
-            int: The number of modified documents.
+            dict: A dictionary containing the operation's success status, message, and result.
         """
-
         collection = self.db[collection_name]
+        update_operation = {}
 
+        # Define the update operation based on the update_type
         if update_type == "set":
             update_operation = {"$set": document_data}
-        if update_type == "push":
+        elif update_type == "push":
             update_operation = {"$push": document_data}
-        if update_type == "pull":
+        elif update_type == "pull":
             update_operation = {"$pull": document_data}
-        if update_type == "unset":
+        elif update_type == "unset":
             update_operation = {"$unset": document_data}
-        if update_type == "inc":
+        elif update_type == "inc":
             update_operation = {"$inc": document_data}
         else:
             return {"success": False, "message": "Invalid update type"}
 
-        result = collection.update_many(query, update_operation)
-        documents_updated_count = result.modified_count
-        return documents_updated_count
+        try:
+            # Perform the update operation
+            result = collection.update_many(query, update_operation)
+            documents_updated_count = result.modified_count
+
+            # Return the number of documents updated
+            if documents_updated_count > 0:
+                return {"success": True, "message": "Documents updated successfully", "result": documents_updated_count}
+            else:
+                return {"success": False, "message": "No documents updated", "result": documents_updated_count}
+
+        except Exception as e:
+            # If an error occurs, return an error message
+            return {"success": False, "message": str(e)}
 
     @handle_db_operations
     def delete(self, query: dict, collection_name: str, many: bool = False) -> int:
@@ -223,7 +232,7 @@ class MongoDB:
         Returns:
             int: The number of deleted documents.
         """
-        # TODO(mongo): Delete Document(s) 
+        # TODO(mongo): Delete Document(s)
         # (fixme, mongo): Implement the method to delete a document or many documents in the database.
         # Refer to MongoDB documentation: https://docs.mongodb.com/manual/tutorial/remove-documents/
 
