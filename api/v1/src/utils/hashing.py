9e55422604168bb9980958521b5e64d7d31ad986
el-tegy <elisee.tegue@groupe-esigelec.org>
2023-11-12 23:38:43 +0100

    feat(api): Add docstring to Hash class in hashing.py

--- api/v1/src/utils/hashing.py
+++ api/v1/src/utils/hashing.py
@@ -4,6 +4,9 @@ pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 class Hash:
+    """
+    A utility class for hashing and verifying passwords using bcrypt algorithm.
+    """
     def bcrypt(password: str):
         return pwd_cxt.hash(password)
 
