849a61782185b45f6c3e5df13ccf0ca3e37c7a9e
el-tegy <elisee.tegue@groupe-esigelec.org>
2023-11-12 23:41:03 +0100

    Refactor: Refactor imports and update method calls in test_user_service.py

--- api/v1/tests/services/test_user_service.py
+++ api/v1/tests/services/test_user_service.py
@@ -1,13 +1,12 @@
 import pytest
-from src.dependencies import get_mongo_db
-from src.services.users_service import UserService  # Replace with the actual import path
+from api.v1.src.dependencies import get_mongo_db
+from api.v1.src.services.users_service import UserService  # Replace with the actual import path
 
 # Setup MongoDB test database
 @pytest.fixture(scope='module')
 def db_instance():
-    db = get_mongo_db('test_db')
+    db = get_mongo_db(database_name='test_db')
     yield db
-
 
 # Setup UserService instance
 @pytest.fixture(scope='module')
@@ -17,7 +16,7 @@ def user_service_instance(db_instance):
 @pytest.mark.user_service
 def test_create_user(user_service_instance):
     user_data = {"username": "John", "password": "secure_password"}
-    user_id = user_service_instance.create_user(user_data).get("result")
+    user_id = user_service_instance.create_user(user_data).get("result").get("result")
     assert isinstance(user_id, str)
     #assert user id is a mongo id string
     assert len(user_id) == 24
@@ -26,7 +25,7 @@ def test_create_user(user_service_instance):
 def test_authenticate_user(user_service_instance):
     username = "John"
     password = "secure_password"
-    user = user_service_instance.authenticate_user(username, password).get("result")
+    user = user_service_instance.authenticate_user(username, password).get("result").get("result")
     assert 'username' in user
     assert user['username'] == username
 
@@ -34,8 +33,8 @@ def test_authenticate_user(user_service_instance):
 def test_read_user(user_service_instance):
     # Assume user_id is obtained from test_create_user or setup
     user_data = {"username": "John", "password": "secure_password"}
-    user_id = user_service_instance.create_user(user_data).get("result")
-    user = user_service_instance.read_user(user_id).get("result")
+    user_id = user_service_instance.create_user(user_data).get("result").get("result")
+    user = user_service_instance.read_user(user_id).get("result").get("result")
     assert '_id' in user
     assert str(user['_id']) == user_id
 
