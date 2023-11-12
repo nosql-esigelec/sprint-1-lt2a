66cdb97db40eb4c2e9bceb448d1d41dc1b3f1008
el-tegy <elisee.tegue@groupe-esigelec.org>
2023-11-12 23:39:44 +0100

    feat(api): Refactor OAuth module to import verify_token from correct location

--- api/v1/src/utils/oauth.py
+++ api/v1/src/utils/oauth.py
@@ -1,11 +1,23 @@
 from fastapi import Depends, HTTPException, status
-from .jwttoken import verify_token
+from api.v1.src.utils.jwttoken import verify_token
 from fastapi.security import OAuth2PasswordBearer
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
+    """
+    Returns the current user based on the provided token.
+
+    Args:
+        token (str): The token to be verified.
+
+    Raises:
+        HTTPException: If the credentials cannot be validated.
+
+    Returns:
+        dict: The user information.
+    """
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
