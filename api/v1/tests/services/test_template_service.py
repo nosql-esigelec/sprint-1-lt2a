949232b64469dde595822923a27873935be580e9
el-tegy <elisee.tegue@groupe-esigelec.org>
2023-11-15 12:29:55 +0100

    fix: Fix tests for TemplateService

--- api/v1/tests/services/test_template_service.py
+++ api/v1/tests/services/test_template_service.py
@@ -25,7 +25,7 @@ def template_service_instance(mongo_instance#, neo4j_instance
                             ):
     return TemplateService(mongo=mongo_instance#, neo4j=neo4j_instance
                         )
-    
+
 @pytest.fixture(scope='module')
 def user_service_instance(mongo_instance):
     yield UserService(db=mongo_instance)
@@ -109,29 +109,36 @@ def test_delete_template(template_service_instance):
     assert deleted_template is None
 
 @pytest.mark.template_service
-def test_delete_private_template(template_service_instance, user_id):
-    # You need to create a template first to test the delete operation
+def test_delete_private_template(template_service_instance, user_service_instance, user_id):
+    # Generate a random user ID specific to this test so that it doesn't interfere with user Ids other tests
+    #data = {"username": "John", "password": "secure_password"}
+    #user_id = user_service_instance.create_user(user_data=data).get("result")
+    
     template_data = {"template_name": "DeleteTest"+random_string(), "is_private": True, "created_by": user_id}
     template_id = template_service_instance.create_template(template_data).get("result")['id']['template_id']
-    
+
     delete_result = template_service_instance.delete_template(template_id, user_id=user_id).get("result")
-    
+
     # Check if the delete operation returned the correct template ID
     assert delete_result == template_id
     # Verify the template no longer exists in the 'templates' collection
     deleted_template = template_service_instance.read_template(template_id, user_id=user_id).get("result")
-    assert deleted_template is None
+    assert deleted_template == None
 
 @pytest.mark.template_service
-def test_star_template(template_service_instance, user_service_instance, user_id):
+def test_star_template(template_service_instance, user_service_instance):
     # You need to create a template first to test the star operation
+    # Generate a random user ID specific to this test so that it doesn't interfere with user Ids other tests
+    data = {"username": "John", "password": "secure_password"}
+    user_id = user_service_instance.create_user(user_data=data).get("result")
+
     template_name = "StarTest"+random_string()
     template_data = {"template_name": template_name, "is_private": True, "created_by": user_id}
-    template_id = template_service_instance.create_template(template_data).get("result")['id']['template_id']
-    # Perform the star operation
-    template_service_instance.star_template(user_id, template_id).get("result")
+    template_id = template_service_instance.create_template(template_data).get("result").get("id")['template_id']
+    # Perform the star operation]
+    template_service_instance.star_template(user_id, template_id)
     # Verify the template is starred by the user
-    starred_template = user_service_instance.read_user(user_id).get("result")#.get("starred_templates")
+    starred_template = user_service_instance.read_user(user_id).get("result").get("starred_templates")
     
     assert starred_template is not None
-    assert starred_template == 'bonjour'    
+    assert starred_template[0]["template_name"] == template_name
